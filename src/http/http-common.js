fcb0e3ecc7233985a0e51f5efa996f0ddb41d79c
phamnguyenkhang01 <60402428+phamnguyenkhang01@users.noreply.github.com>
2022-03-08 22:06:11 -0600

    Delete src directory

--- src/http/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-import authHeader from '../helper/auth-header.js';
-
-export default axios.create({
-    baseURL: "http://localhost:8080/",
-    headers:  authHeader()
-});
