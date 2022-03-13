7ea2348f6e184549e098ed4658eaebc39ae97a0f
mnguye85 <jacksonnguyen2597@gmail.com>
2022-03-13 14:57:15 -0500

    new unit testing

--- back-end/app/models/model.test.js
+++ back-end/app/models/model.test.js
@@ -2,7 +2,7 @@ const { sequelize } = require('.')
 const usermodel = require('./user.model')
 
 test('test user model', () =>{
-    expect(usermodel(sequelize.define(usermodel))).toBe(sequelize.define(usermodel))
+    // test user model here
 })
 
 //('name@email.com', '', 'name@email', '10 street')
--- back-end/app/quotecalculator.test.js
+++ back-end/app/quotecalculator.test.js
@@ -1,5 +1,9 @@
 const qc = require ('./quotecalculator')
 
 test('test quote calculator', () => {
-    expect(qc.quotecalculator(1.5, 0.02, 0.01, 0.02, 0.10))
+    expect(qc.margin(1.5, 0.02, 0.01, 0.02, 0.10)).toBe(.195)
+
+    margin = qc.margin(1.5, 0.02, 0.01, 0.02, 0.10)
+    expect(qc.totaldue(1500, margin)).toBe(2542.5)
+
 })
