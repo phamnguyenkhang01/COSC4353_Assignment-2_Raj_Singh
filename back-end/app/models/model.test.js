8980b2add6f66d53499ba1f25e2ea2825442aac0
mnguye85 <jacksonnguyen2597@gmail.com>
2022-03-30 23:36:00 -0500

    user model unit test

--- back-end/app/models/model.test.js
+++ back-end/app/models/model.test.js
@@ -1,8 +1,27 @@
-const { sequelize } = require('.')
-const usermodel = require('./user.model')
+//const { sequelize } = require('.')
+//const usermodel = require('./user.model')
+//const { DESCRIBE } = require('sequelize/types/query-types')
+const { expect } = require('chai')
 
-test('test user model', () =>{
-    // test user model here
+const {
+    sequelize,
+    dataTypes,
+    checkModelName,
+    checkPropertyExists
+  } = require('sequelize-test-helpers')
+
+const UserModel = require('../models/user.model')
+
+describe('../models/user.model', () => {
+    const User = UserModel(sequelize, dataTypes)
+    const user = new User()
+
+    checkModelName(User)('users') // check new user instance is created
+
+    // context('properties', () => {
+    //     ;['id', 'email', 'password', 'full_name', 'address_line_1', 'address_line_2', 'city', 'state', 'state', 'zip_code', 'status']
+    //     .forEach(checkPropertyExists(user))
+    // })
+
+    
 })
-
-//('name@email.com', '', 'name@email', '10 street')
