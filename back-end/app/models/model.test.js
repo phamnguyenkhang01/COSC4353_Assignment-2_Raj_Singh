dce5ba8e3b4c470b2200920b8cf84ffa2d2b08ab
mnguye85 <jacksonnguyen2597@gmail.com>
2022-03-31 18:22:29 -0500

    add quote model unit tests

--- back-end/app/models/model.test.js
+++ back-end/app/models/model.test.js
@@ -1,14 +1,15 @@
-//const { sequelize } = require('.')
-//const usermodel = require('./user.model')
-//const { DESCRIBE } = require('sequelize/types/query-types')
-const { expect } = require('chai')
+// use 
+// npm i -D sequelize-test-helpers
+// to install test dependencies
 
 const {
     sequelize,
     dataTypes,
     checkModelName,
-    checkPropertyExists
+    checkPropertyExists,
+    //checkUniqueIndex
   } = require('sequelize-test-helpers')
+
 
 const UserModel = require('../models/user.model')
 
@@ -16,12 +17,26 @@ describe('../models/user.model', () => {
     const User = UserModel(sequelize, dataTypes)
     const user = new User()
 
-    checkModelName(User)('users') // check new user instance is created
+    checkModelName(User)('users'); // check new user instance is created
 
-    // context('properties', () => {
-    //     ;['id', 'email', 'password', 'full_name', 'address_line_1', 'address_line_2', 'city', 'state', 'state', 'zip_code', 'status']
-    //     .forEach(checkPropertyExists(user))
-    // })
+    // check user model's properties
+    ['id', 'email', 'password', 'full_name', 'address_line_1', 'address_line_2', 'city', 'state', 'state', 'zip_code', 'status']
+        .forEach(checkPropertyExists(user));
+
+
+})
+
+const QuoteModel = require('../models/quote.model')
+
+describe('../models/quote.model', () => {
+    const Quote = QuoteModel(sequelize, dataTypes)
+    const quote = new Quote()
+
+    checkModelName(Quote)('quotes');
+
+    // check quote model's properties
+    ['id', 'gallons', 'rate', 'delivery_address', 'delivery_date', 'quote_date', 'request_by']
+        .forEach(checkPropertyExists(quote));
 
     
 })
